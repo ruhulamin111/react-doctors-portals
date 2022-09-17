b381a34d381d06dd19564f0e3d9b8c57e6ca8d07
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-09-17 08:12:00 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 import './App.css';
+import Home from './pages/Home/Home';
+import { Routes, Route } from "react-router-dom";
+import Navbar from './pages/Navbar/Navbar';
+
 
 function App() {
   return (
     <div>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+
+
+      </Routes>
 
     </div>
   );
