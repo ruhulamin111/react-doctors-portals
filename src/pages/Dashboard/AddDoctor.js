03a8117125972076515edaa8fc8f7ae6fc21a5a4
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-02 08:23:04 +0600

    image upload

--- src/pages/Dashboard/AddDoctor.js
+++ src/pages/Dashboard/AddDoctor.js
@@ -1,59 +1,106 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import Loading from '../Loading/Loading';
+import { useForm } from "react-hook-form";
 
 const AddDoctor = () => {
+    const { data: service, isLoading } = useQuery('service', () => fetch('http://localhost:5000/services').then(res => res.json()));
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const imagekey = '79c13bff4cf174d3aee764506ad7de92';
 
-    const { data: service, isLoading, prefetch } = useQuery('service', () => fetch('http://localhost:5000/services').then(res => res.json()));
+    const onSubmit = async data => {
+        const image = data.image[0];
+        const formData = new FormData();
+        formData.append('image', image);
+        const url = `https://api.imgbb.com/1/upload?key=${imagekey}`
+        fetch(url, {
+            method: 'POST',
+            body: formData,
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.success) {
+                    const img = data.url;
+                    const doctor = {
+                        name: data.name,
+                        email: data.email,
+                        service: data.service,
+                        img: img,
+                    }
 
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const name = event.target.name.value;
-        const email = event.target.email.value;
-        const service = event.target.service.value;
-        const file = event.target.file.value;
-        console.log(name, email, service, file);
-        event.target.reset();
+                }
+            })
     }
+
     if (isLoading) {
         return <Loading></Loading>
     }
-
 
     return (
         <div className='flex justify-center items-center'>
             <div className="card w-96 bg-base-100 shadow-xl py-4">
                 <div className="card-body items-center text-center">
                     <h2 className="card-title mb-4">Add a doctor</h2>
-                    <form onSubmit={handleSubmit} className='w-full'>
+
+                    <form onSubmit={handleSubmit(onSubmit)} className='w-full'>
                         <div className="form-control ">
                             <label className="label">
                                 <span className="label-text">Name </span>
                             </label>
-                            <input type="text" placeholder="Name" className="input input-bordered w-full " required name='name' />
+                            <input type="text" placeholder="Name" className="input input-bordered w-full "
+                                {...register("name", {
+                                    required: { value: true, message: 'Name is required' }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.name?.type === 'required' && <span className="label-text-alt">{errors.name.message}</span>}
+                            </label>
                         </div>
+
                         <div className="form-control w-full max-w-xs">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs" required name='email' />
+                            <input type="email" placeholder="Email" className="input input-bordered w-full max-w-xs"
+                                {...register("email", {
+                                    required: { value: true, message: 'Email is required' }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.email?.type === 'required' && <span className="label-text-alt">{errors.password.message}</span>}
+                            </label>
                         </div>
+
                         <div className="form-control w-full max-w-xs">
                             <label className="label">
-                                <span className="label-text">Service </span>
+                                <span className="label-text">Service</span>
                             </label>
-                            <select name='service' className="select select-bordered w-full max-w-xs">
+                            <select type="file" className="input input-bordered w-full max-w-xs"
+                                {...register("service", {
+                                    required: { value: true, message: 'File  is required' }
+                                })}
+                            >
                                 {service.map((item, index) => <option
                                     key={index}
                                     value={item.name}
                                 >{item.name}</option>)}
                             </select>
+                            <label className="label">
+                                {errors.service?.type === 'required' && <span className="label-text-alt">{errors.service.message}</span>}
+                            </label>
                         </div>
                         <div className="form-control w-full max-w-xs">
                             <label className="label">
                                 <span className="label-text">Photo</span>
                             </label>
-                            <input type="file" className="input input-bordered w-full max-w-xs" required name='file' />
+                            <input type="file" className="input input-bordered w-full max-w-xs"
+                                {...register("image", {
+                                    required: { value: true, message: 'File  is required' }
+                                })}
+                            />
+                            <label className="label">
+                                {errors.image?.type === 'required' && <span className="label-text-alt">{errors.image.message}</span>}
+                            </label>
                         </div>
 
                         <input type='submit' value='Add' className="btn btn-accent w-full mt-4"></input>
