61d64b69a209a05ec4c59caa9a7166593e2e6571
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-10-02 22:42:34 +0600

    stripe add

--- src/pages/Dashboard/Payment.js
+++ src/pages/Dashboard/Payment.js
@@ -1,15 +1,37 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
+import Loading from '../Loading/Loading';
 
 const Payment = () => {
-    const { id } = useParams()
+    const { id } = useParams();
+    const url = `http://localhost:5000/bookings/${id}`
+    const { data: booking, isLoading } = useQuery(['bookings', id], () => fetch(url, {
+        method: 'GET',
+        headers: {
+            'authorization': `${localStorage.getItem('token')}`,
+        }
+    }).then(res => res.json()))
 
+    if (isLoading) {
+        return <Loading></Loading>
+    }
 
+    console.log(booking);
     return (
         <div>
-            <h1>hello</h1>
-            <p>{id}</p>
-
+            <div className="card w-96 bg-base-100 shadow-xl">
+                <div className="card-body">
+                    <h2 className="card-title">Payment Method</h2>
+                    <p>Service: {booking.serviceName}</p>
+                    <p>Price: ${booking.price}</p>
+                    <p>Email: {booking.email}</p>
+                    <p>Patient: {booking.patientName}</p>
+                    <div className="card-actions justify-end">
+                        <button className="btn btn-primary">Buy Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
